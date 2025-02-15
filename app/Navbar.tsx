5048651c48cb9fb4abc74a063b3b535929537da2
max-carver <maxevans3108@gmail.com>
2025-02-15 06:51:30 +0200

    Add authentication and Prisma dependencies, update Navbar with logo

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,14 +3,19 @@
 import { Container } from "@/components/container";
 import ThemeToggle from "@/components/ThemeToggle";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/mealplanlogo.svg";
 
 export default function Navbar() {
   return (
     <Container>
       <nav className=" flex justify-between items-center h-16">
-        <Link href="/" className="text-2xl font-bold">
-          Meal<span className="text-primary">PlannerAI</span>
+        <Link href="/" className="text-3xl font-bold flex items-center gap-0.5">
+          <Image src={logo} alt="logo" width={25} height={25} />
+          <div>
+            Meal<span className="text-primary">PlannerAI</span>
+          </div>
         </Link>
         <Button variant="outline">Outline</Button>
         <Button variant="default">Default</Button>
